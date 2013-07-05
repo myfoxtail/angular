bdf34920a7097ece6a19c04494186bf1021ce496
myfoxtail <alexandramourzina@gmail.com>
2013-07-05 16:53:13 +0400

    v. 0.1

---
+++ directives/placeholder.js
@@ -0,0 +1,24 @@
+PH = angular.module('PH', []);
+
+PH.directive('placeholder', function(){
+	// you should fine out by yourself browser data and browser version
+	if( browser != "Explorer" || version >= 10) return false;
+	var directiveName = 'placeholder';
+	return {
+		link: function(scope, elm, attrs){
+			var value = attrs[directiveName];
+			attrs.ngModel = value;
+
+			elm.bind('focus', function(){
+				if( attrs.ngModel === value) {
+					scope.$apply(function(){attrs.ngModel = ''});
+				}
+			});
+			elm.bind('blur', function(){
+				if( attrs.ngModel === '' ) {
+					scope.$apply(function(){attrs.ngModel = value});
+				}
+			});
+		}
+	}
+});
